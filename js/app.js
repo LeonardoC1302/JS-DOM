f6bac1637751188a35f9ea5ab2d9d1dd70f23872
LeonardoC1302 <lecespedes@estudiantec.cr>
2023-05-01 14:46:07 -0600

    Query Selector

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Select content
+const heading = document.querySelector('.header__texto h2'); // Returns 0 or 1 element
+console.log(heading);
+heading.textContent = 'New Heading';
+
+// Query Selector
+
+// Query Selector All
+
+// Get Element By ID
